a295673a1c8a6bc9a27ebf36f7c5a6a0b490ce9f
Adam Graham <adam.zigurous@gmail.com>
2020-10-03 01:56:01 -0500

    Remove React.StrictMode

--- src/index.js
+++ src/index.js
@@ -9,12 +9,7 @@ import './styles/typography.css';
 import './styles/objects.css';
 import './styles/spacing.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
